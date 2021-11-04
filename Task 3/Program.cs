b71dd4f5d4cfeb3608e972526ae5e7c531ecf1dd
Aleksei Chikin <lehafar@gmail.com>
2021-11-04 17:42:22 +0200

    add task 3

---
+++ Task 3/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+
+namespace Task_3
+{
+    class Program
+    {
+        static Triangle CreateTriangle()
+        {
+            string[] tmp = new string[4];
+            string name;
+            double a, b, c;
+            Triangle triangle;
+
+            Console.WriteLine("Name = ");
+            tmp = Console.ReadLine().Split(',');
+
+            name = tmp[0].Trim().ToLower();
+            Double.TryParse(tmp[1], out a);
+            Double.TryParse(tmp[2], out b);
+            Double.TryParse(tmp[3], out c);
+            triangle = new Triangle(name, a, b, c);
+
+            if (!triangle.isValid)
+                return null;
+
+            return triangle;
+        }
+        static void Main()
+        {
+            SortedList srtLst = new SortedList();
+
+            Triangle trgl;
+
+            while (true)
+            {
+                trgl = CreateTriangle();
+                if (trgl == null)
+                {
+                    Console.WriteLine("Triangle uncorrect, try anothe one");
+                    continue;
+                }
+                srtLst.Add(trgl.Area, trgl);
+                Console.WriteLine("Triangle added!");
+                Console.WriteLine("Anothe one ?");
+                string userResponse = Console.ReadLine().ToLower();
+
+                if (userResponse == "y" || userResponse == "yes")
+                    continue;
+                else
+                    break;
+            }
+
+            Console.WriteLine("============= Triangles list: ===============");
+
+            for (int i = 0; i < srtLst.Count; i++)
+                Console.WriteLine($"{i}. [Triangle {srtLst.GetByIndex(i)}]: {srtLst.GetKey(i)} cm^3");
+
+
+        }
+    }
+}
+
---
+++ Task 3/Triangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Triangle
+    {
+        public string Name { get; init; }
+        public double SideA { get; init; }
+        public double SideB { get; init; }
+        public double SideC { get; init; }
+        public double Area { get; init; }
+        public bool isValid { get; init; }
+        public Triangle(string name, double a, double b, double c)
+        {
+            Name = name;
+            SideA = a;
+            SideB = b;
+            SideC = c;
+            Area = AreaOfATriangle();
+            isValid = CheckValidity();
+        }
+        private bool CheckValidity()
+        {
+            return !Double.IsNaN(Area);
+        }
+        private double AreaOfATriangle()
+        {
+            //Heron's formula
+            double p = (SideA + SideB + SideC) / 2;
+            double result = Math.Sqrt(p * (p - SideA) * (p - SideB) * (p - SideC));
+
+            return result;
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
