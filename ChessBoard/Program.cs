42e48e100d83581904b363cda11ddb5abc872560
Aleksei Chikin <lehafar@gmail.com>
2021-11-04 17:31:48 +0200

    add chessboard class with basic business logic

--- ChessBoard/Program.cs
+++ ChessBoard/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 
-namespace ChessBoard
+namespace Task1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ChessBoard board = new ChessBoard(5, 5);
+            Console.WriteLine(board);
+
+            //ushort height, width;
+            //if (ushort.TryParse(Console.ReadLine(), out height) &&
+            //   ushort.TryParse(Console.ReadLine(), out width))
+            //{
+            //    ChessBoard board = new ChessBoard(height, width);
+            //    Console.WriteLine(board);
+            //}
         }
     }
 }
