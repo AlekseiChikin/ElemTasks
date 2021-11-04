ff7a8710ca70b02151685967c8f45dd5ba8ff57d
Aleksei Chikin <lehafar@gmail.com>
2021-11-04 17:37:43 +0200

    add class envelope

---
+++ Task 2/Envelope.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Envelope
+    {
+        public Envelope(double a, double b)
+        {
+            SideA = a;
+            SideB = b;
+        }
+        public double SideA { get; init; }
+        public double SideB { get; init; }
+        public bool CanBeInsertedIn(Envelope env)
+        {
+            if (SideA < env.SideA && SideB < env.SideB)
+                return true;
+            if (SideA < env.SideB && SideB < env.SideA)
+                return true;
+
+            return false;
+        }
+    }
+}
--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -4,9 +4,31 @@ namespace Task_2
 {
     class Program
     {
-        static void Main(string[] args)
+        static Envelope CreateEnvelop(string sideA, string sideB)
         {
-            Console.WriteLine("Hello World!");
+            double _sideA, _sideB;
+            while (true)
+            {
+                Console.Write($"Envelop ({sideA},{sideB}) side {sideA} = ");
+                if (!Double.TryParse(Console.ReadLine(), out _sideA))
+                {
+                    Console.WriteLine("invalid argument, try again");
+                    continue;
+                }
+                Console.Write($"Envelop ({sideA},{sideB}) side {sideB} = ");
+                if (!Double.TryParse(Console.ReadLine(), out _sideB))
+                    continue;
+
+                return new Envelope(_sideA, _sideB);
+            }
+        }
+        static void Main()
+        {
+            Envelope env1 = CreateEnvelop("a", "b");
+            Envelope env2 = CreateEnvelop("c", "d");
+
+            Console.WriteLine(env1.CanBeInsertedIn(env2));
+
         }
     }
 }
